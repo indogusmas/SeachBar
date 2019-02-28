f1bf44687b50dbbf482add986de4ea5e797ed93a
indo <indogusmasarungsamudra@gmail.com>
2019-02-28 10:17:32 +0700

    Initial Commit

---
+++ SearchBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBar
+//
+//  Created by indo gusmas arung samudra on 28/02/19.
+//  Copyright © 2019 indo gusmas arung samudra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
